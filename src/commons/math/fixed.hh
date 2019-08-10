e51828f7143a772043c96fe78eaedd8ae780a15c
Izzy Axel <izzoriousaxel@gmail.com>
2019-08-10 03:46:37 -0400

    Start fixed point math class

---
+++ src/commons/math/fixed.hh
@@ -0,0 +1,319 @@
+#pragma once
+
+#include <cstdint>
+#include <cmath>
+
+//Fixed point number
+template <uint32_t bits, typename V> struct fixed
+{
+	static_assert(std::is_integral<V>() && bits <= sizeof(V) * 8);
+	
+	#if defined(__MINGW32__) || defined(__clang__)
+	using int128_t = __int128_t;
+	using uint128_t = __uint128_t;
+	#elif defined(__MSC_VER)
+	using int128_t = __int128;
+	using uint128_t = __uint128;
+	#endif
+	
+	fixed() = default;
+	constexpr explicit inline fixed(float val) : val(std::round(val * this->scale)) {}
+	constexpr explicit inline fixed(double val) : val(std::round(val * this->scale)) {}
+	constexpr explicit inline fixed(uint8_t val) : val(val * this->scale) {}
+	constexpr explicit inline fixed(uint16_t val) : val(val * this->scale) {}
+	constexpr explicit inline fixed(uint32_t val) : val(val * this->scale) {}
+	constexpr explicit inline fixed(uint64_t val) : val(val * this->scale) {}
+	constexpr explicit inline fixed(int8_t val) : val(val * this->scale) {}
+	constexpr explicit inline fixed(int16_t val) : val(val * this->scale) {}
+	constexpr explicit inline fixed(int32_t val) : val(val * this->scale) {}
+	constexpr explicit inline fixed(int64_t val) : val(val * this->scale) {}
+	
+	//Conversion
+	constexpr explicit inline operator float() const
+	{
+		return this->toFloat();
+	}
+	
+	constexpr explicit inline operator double() const
+	{
+		return this->toDouble();
+	}
+	
+	//Bool
+	constexpr inline operator bool() const
+	{
+		return this->val != 0;
+	}
+	
+	//Comparison //TODO unfinished
+	template <uint32_t obits, typename T> constexpr bool operator ==(fixed<obits, T> other) const
+	{
+		if(bits != obits)
+		{
+			
+		}
+		return (obits == this->scale && other.val == this->val) || this->toDouble() == other.toDouble();
+	}
+	
+	template <uint32_t obits, typename T> constexpr inline bool operator !=(fixed<obits, T> other) const
+	{
+		return !this->operator==(other);
+	}
+	
+	template <uint32_t obits, typename T> constexpr inline bool operator <(fixed<obits, T> other) const
+	{
+		if(bits != obits)
+		{
+			
+		}
+		return this->val < other.val;
+	}
+	
+	template <uint32_t obits, typename T> constexpr inline bool operator >(fixed<obits, T> other) const
+	{
+		if(bits != obits)
+		{
+			
+		}
+		return this->val > other.val;
+	}
+	
+	template <uint32_t obits, typename T> constexpr inline bool operator <=(fixed<obits, T> other) const
+	{
+		if(bits != obits)
+		{
+			
+		}
+		return this->val <= other.val;
+	}
+	
+	template <uint32_t obits, typename T> constexpr inline bool operator >=(fixed<obits, T> other) const
+	{
+		if(bits != obits)
+		{
+			
+		}
+		return this->val >= other.val;
+	}
+	
+	//Math
+	constexpr inline fixed<bits, V> operator -() const
+	{
+		this->val = -this->val;
+		return *this;
+	}
+	
+	constexpr inline fixed<bits, V> operator +(fixed<bits, V> other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val + other.val;
+		return out;
+	}
+	constexpr inline fixed<bits, V> operator +=(fixed<bits, V> other) const
+	{
+		this->val = this->val + other.val;
+		return *this;
+	}
+	
+	constexpr inline fixed<bits, V> operator -(fixed<bits, V> other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val - other.val;
+		return out;
+	}
+	constexpr inline fixed<bits, V> operator -=(fixed<bits, V> other) const
+	{
+		this->val = this->val - other.val;
+		return *this;
+	}
+	
+	constexpr inline fixed<bits, V> operator *(fixed<bits, V> other) const
+	{
+		fixed<bits, V> out;
+		out.val = ((int128_t)this->val * (int128_t)other.val) / this->scale;
+		return out;
+	}
+	constexpr inline fixed<bits, V> operator *=(fixed<bits, V> other) const
+	{
+		this->val = ((int128_t)this->val * (int128_t)other.val) / this->scale;
+		return *this;
+	}
+	
+	constexpr inline fixed<bits, V> operator /(fixed<bits, V> other) const
+	{
+		fixed<bits, V> out;
+		out.val = ((int128_t)this->val * this->scale) / (int128_t)other.val;
+		return out;
+	}
+	constexpr inline fixed<bits, V> operator /=(fixed<bits, V> other) const
+	{
+		this->val = ((int128_t)this->val * this->scale) / (int128_t)other.val;
+		return *this;
+	}
+	
+	constexpr inline fixed<bits, V> operator %(fixed<bits, V> other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val % other.val;
+		return out;
+	}
+	constexpr inline fixed<bits, V> operator %=(fixed<bits, V> other) const
+	{
+		this->val = this->val % other.val;
+		return *this;
+	}
+	
+	template <typename T> constexpr fixed<bits, V> operator +(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		return *this + fixed<bits, T>{other};
+	}
+	template <typename T> constexpr fixed<bits, V> operator +=(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		this->val = (*this + fixed<bits, T>{other}).val;
+		return *this;
+	}
+	
+	template <typename T> constexpr fixed<bits, V> operator -(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		return *this - fixed<bits, T>{other};
+	}
+	template <typename T> constexpr fixed<bits, V> operator -=(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		this->val = (*this - fixed<bits, T>{other}).val;
+		return *this;
+	}
+	
+	template <typename T> constexpr fixed<bits, V> operator *(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		return *this * fixed<bits, T>{other};
+	}
+	template <typename T> constexpr fixed<bits, V> operator *=(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		this->val = (*this - fixed<bits, T>{other}).val;
+		return *this;
+	}
+	
+	template <typename T> constexpr fixed<bits, V> operator /(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		return *this / fixed<bits, T>{other};
+	}
+	template <typename T> constexpr fixed<bits, V> operator /=(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		this->val = this->val / other;
+		return *this;
+	}
+	
+	template <typename T> constexpr fixed<bits, V> operator %(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		return *this % fixed<bits, T>{other};
+	}
+	template <typename T> constexpr fixed<bits, V> operator %=(T other) const
+	{
+		static_assert(std::is_integral<T>() || std::is_floating_point<T>());
+		this->val = (*this % fixed<bits, T>{other});
+		return *this;
+	}
+	
+	//Bitwise
+	constexpr fixed<bits, V> inline operator ~() const
+	{
+		fixed<bits, V> out;
+		out.val = ~this->val;
+		return out;
+	}
+	
+	constexpr fixed<bits, V> inline operator >>(int32_t other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val >> other;
+		return out;
+	}
+	
+	constexpr fixed<bits, V> inline operator <<(int32_t other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val << other;
+		return out;
+	}
+	
+	constexpr fixed<bits, V> inline operator |(int32_t other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val | other;
+		return out;
+	}
+	
+	constexpr fixed<bits, V> inline operator &(int32_t other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val & other;
+		return out;
+	}
+	
+	constexpr fixed<bits, V> inline operator ^(int32_t other) const
+	{
+		fixed<bits, V> out;
+		out.val = this->val ^ other;
+		return out;
+	}
+	
+	constexpr fixed<bits, V> inline operator <<=(int32_t other) const
+	{
+		this->val = this->val << other;
+		return *this;
+	}
+	
+	constexpr fixed<bits, V> inline operator >>=(int32_t other) const
+	{
+		this->val = this->val >> other;
+		return *this;
+	}
+	
+	constexpr fixed<bits, V> inline operator |=(int32_t other) const
+	{
+		this->val = this->val | other;
+		return *this;
+	}
+	
+	constexpr fixed<bits, V> inline operator &=(int32_t other) const
+	{
+		this->val = this->val & other;
+		return *this;
+	}
+	
+	constexpr fixed<bits, V> inline operator ^=(int32_t other) const
+	{
+		this->val = this->val ^ other;
+		return *this;
+	}
+	
+	//Outputs
+	constexpr inline float toFloat() const
+	{
+		return ((float)this->val / this->scale);
+	}
+	
+	constexpr inline double toDouble() const
+	{
+		return ((double)this->val / this->scale);
+	}
+	
+	template <uint32_t nbits> constexpr fixed<nbits, V> changeBits() const
+	{
+		return fixed<nbits, V>{this->toDouble()};
+	}
+	
+private:
+	static constexpr uint32_t const scale = 1 << bits;
+	V val;
+};
+static_assert(std::is_pod<fixed<8, int32_t>>());
