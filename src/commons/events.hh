6a712811e07aea38a3cda47839ea587b4c3f67b1
Izzy Axel <bencosgriff@gmail.com>
2022-03-09 14:33:38 -0500

    Add events documentation

--- src/commons/events.hh
+++ src/commons/events.hh
@@ -24,11 +24,18 @@
 /// The way this is set up allows multiple events with the same parameters to be treated as separate events
 template <typename ... Ts> struct EventBus
 {
+	/// Register an event handler
+	/// \tparam Event The type of event to register
+	/// \param event The event callback and data
 	template <typename Event> void registerEventHandler(Event const &event)
 	{
 		std::get<std::vector<Event>>(this->registrars).push_back(event);
 	}
 	
+	/// Activate a given event's handlers
+	/// \tparam Event The type of event to post
+	/// \tparam Args The type of the arguments
+	/// \param args The arguments to pass into the callbacks that handle this event type
 	template <typename Event, typename ... Args> void post(Args ... args)
 	{
 		for(auto const &event : std::get<std::vector<Event>>(this->registrars)) event(args ...);
